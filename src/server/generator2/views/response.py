3e5704e2cdfdb076c55ea14d75da4c7e7f438a30
Christopher Haverman <chaverma@alumni.uci.edu>
2017-09-05 22:56:28 -0700

    Added a representation of our API responses.

---
+++ src/server/generator2/views/response.py
@@ -0,0 +1,32 @@
+# Copyright 2017 Christopher Haverman
+# All Rights Reserved
+#
+__author__ = 'Christopher Haverman'
+
+
+class ApiResponse:
+    """
+    Value object designed to formalize this API's response schema.
+    """
+    ERROR = 'error'
+    OK = 'ok'
+
+    def __init__(self, **kwargs):
+        """
+        :keyword message: A UI-facing error message, or empty.
+        :keyword objects: Iterable of objects (in the domain sense).
+        :keyword order: Iterable of string keys of ``objects``.
+        :keyword status: Either "error" (default) or "ok".
+        """
+        self.message = kwargs.get('message', '')
+        self.objects = kwargs.get('objects', [])
+        self.status = kwargs.get('status', self.ERROR)
+
+    def __json__(self, request):
+        return {
+            'status': {
+                'code': self.status,
+                'message': self.message
+            },
+            'objects': self.objects
+        }
