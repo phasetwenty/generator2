780c427ca8f1b7b6b40c2cc7515e311d3ab29698
Christopher Haverman <chaverma@alumni.uci.edu>
2017-09-24 20:58:44 -0700

    Updated JS tests to incorporate Jest snapshots rather than tedious low-level inspection.

--- src/client/tests/components/MainNav.spec.js
+++ src/client/tests/components/MainNav.spec.js
@@ -4,8 +4,9 @@
  **/
 /* global beforeEach, describe, expect, test */
 import MainNav, {MainNavItem, MainNavSection} from '../../components/MainNav';
+import {MemoryRouter} from 'react-router-dom';
 import React from 'react';
-import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
 
 describe('MainNav', () => {
   const items = [
@@ -14,38 +15,13 @@ describe('MainNav', () => {
       "subcategories": [{"name": "Second-level", "items": [["Thieves"], ["Nobles"]]}]
     }
   ];
-  let componentUnderTest = null;
 
-  beforeEach(() => {
-    componentUnderTest = shallow(<MainNav items={items}/>);
-  });
-
-  test('renders the expected number of category sections.', () => {
-    const categoryElements = componentUnderTest.find(MainNavSection);
-    expect(categoryElements.length).toBe(2);
-  });
-
-  test('renders a category section.', () => {
-    const categoryElement = componentUnderTest
-      .find(MainNavSection)
-      .findWhere(node => node.render().text() === 'Top-level');
-    expect(categoryElement.length).toBe(1);
-  });
-
-  test('renders a subcategory section.', () => {
-    const subcategoryElement = componentUnderTest
-      .find(MainNavSection)
-      .findWhere(node => node.render().text() === 'Second-level');
-    expect(subcategoryElement.length).toBe(1);
-  });
-
-  test('renders the leaf nav items.', () => {
-    const navItems = componentUnderTest.find(MainNavItem);
-
-    const thievesItem = navItems.findWhere(node => node.render().text() === 'Thieves');
-    expect(thievesItem.length).toBe(1);
-
-    const noblesItem = navItems.findWhere(node => node.render().text() === 'Nobles');
-    expect(noblesItem.length).toBe(1);
+  test('matches snapshot', () => {
+    const currentSnapshot = renderer.create(
+        <MemoryRouter>
+          <MainNav items={items}/>
+        </MemoryRouter>
+    ).toJSON();
+    expect(currentSnapshot).toMatchSnapshot();
   });
 });
